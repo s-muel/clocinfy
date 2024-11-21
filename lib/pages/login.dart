cd7ed3678d01473245de241d9124386d5c066456
s-muel <samuel.essuman@amalitech.org>
2024-11-21 15:56:50 +0000

    added login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -10,11 +8,34 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-       children: [Text('Login')],
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Vertically center the children
+                crossAxisAlignment: CrossAxisAlignment
+                    .center, // Horizontally center the children
+                children: [
+                  const Text("LOGIN"),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(labelText: "email"),
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
